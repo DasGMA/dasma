f7441a63ba78835eb38844df88bb34349f8bed4c
DasGMA <dasgrigoma@gmail.com>
2020-05-26 11:26:33 -0700

    Working on projects

--- src/Components/Collection.js
+++ src/Components/Collection.js
@@ -1,33 +1,42 @@
 import space from '../Media/spaceShooter.png';
+import portfolio from '../Media/portfolio.png';
+import lbb from '../Media/lbb.png';
+import hiit from '../Media/hiit.png';
 
 export const collection = [
     {
+        projectName: 'HIIT Training App',
+        projectLink: 'https://github.com/DasGMA/hiit-training',
+        projectImage: hiit,
+        projectDescription: 'HIIT Training APP'
+    },
+    {
+        projectName: 'Long Beach Business',
+        projectLink: 'https://github.com/DasGMA/long-beach-business',
+        projectImage: lbb,
+        projectDescription: `Long Beach Business`
+    },
+    {
+        projectName: 'Portfolio',
+        projectLink: 'https://www.dasma.dev',
+        projectImage: portfolio,
+        projectDescription: `What kind of Developer without a Portfolio Website, right? I believe in saying: 'Simplicity is the soul of efficiency'. Used React JS and SCSS to create simple and responsive design that is easy to navigate and read its contents.`
+    },
+    {
+        projectName: 'Space Shooting Game',
+        projectLink: 'https://guardians-of-the-galaxy-crazy-ugly-space-game.netlify.app/',
+        projectImage: space,
+        projectDescription: 'Space shooting game - May 2019. React | ES6 Classes | Canvas | OOP'
+    },
+    {
+        projectName: 'Simple Calculator',
+        projectLink: '',
+        projectImage: space,
+        projectDescription: 'Space shooting game - May 2019. React | ES6 Classes | Canvas | OOP'
+    },
+    {
         projectName: 'User Interface Project Week - Design Files',
-        projectLink: 'https://guardians-of-the-galaxy-crazy-ugly-space-game.netlify.app/',
-        projectImage: space,
-        projectDescription: 'Space shooting game - May 2019. React | ES6 Classes | Canvas | OOP'
-    },
-    {
-        projectName: 'React Wars',
-        projectLink: 'https://guardians-of-the-galaxy-crazy-ugly-space-game.netlify.app/',
-        projectImage: space,
-        projectDescription: 'Space shooting game - May 2019. React | ES6 Classes | Canvas | OOP'
-    },
-    {
-        projectName: 'User Interface Project Week - Design Files',
-        projectLink: 'https://guardians-of-the-galaxy-crazy-ugly-space-game.netlify.app/',
-        projectImage: space,
-        projectDescription: 'Space shooting game - May 2019. React | ES6 Classes | Canvas | OOP'
-    },
-    {
-        projectName: 'Web Tools and Frameworks',
-        projectLink: 'https://guardians-of-the-galaxy-crazy-ugly-space-game.netlify.app/',
-        projectImage: space,
-        projectDescription: 'Space shooting game - May 2019. React | ES6 Classes | Canvas | OOP'
-    },
-    {
-        projectName: 'UI - Responsive',
-        projectLink: 'https://guardians-of-the-galaxy-crazy-ugly-space-game.netlify.app/',
+        projectLink: '',
         projectImage: space,
         projectDescription: 'Space shooting game - May 2019. React | ES6 Classes | Canvas | OOP'
     }
