dd7df31fe55923bf439e95d53d373f5d947f67d2
Das Ma <dasgrigoma@gmail.com>
2021-03-01 11:37:36 -0800

    Little changes.

--- src/Components/Jumbos/ProjectsJumbo.js
+++ src/Components/Jumbos/ProjectsJumbo.js
@@ -5,7 +5,7 @@ export default function ProjectsJumbo() {
     return (
         <div className='projects-jumbo'>
             <a href='/projects'><h1>Projects</h1></a>
-            <h2>Here is a <span>Collection</span> of my <span>Projects</span>.</h2>
+            <h2>Here is a little <span>Collection</span> of my <span>Projects</span>.</h2>
             <h2>A <span>Journey</span> to where i am now.</h2>
             <h2>For more checkout my <span><a href = 'https://github.com/DasGMA' target = '_blank' rel = 'noopener noreferrer'>GitHub</a></span>.</h2>
         </div>
